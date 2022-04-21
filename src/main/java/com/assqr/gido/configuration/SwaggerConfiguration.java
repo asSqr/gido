57386fbb3236bf19af9d5ccba054a3a90f037798
Ysmr-Ry <ryo.yossy@live.jp>
2022-04-21 22:19:31 +0900

    del: 不要な import

--- src/main/java/com/assqr/gido/configuration/SwaggerConfiguration.java
+++ src/main/java/com/assqr/gido/configuration/SwaggerConfiguration.java
@@ -3,8 +3,6 @@ package com.assqr.gido.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
